a377b4d14c6d7abc4df4b544b1dd850b05dba6d8
ErinJordan222 <erin.jordan@du.edu>
2022-06-13 10:56:23 -0600

    first_setup

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import NavTabs from './NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
