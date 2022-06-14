9c23648374934c9827229e0ac01763050e3726c9
ErinJordan222 <erin.jordan@du.edu>
2022-06-14 15:54:28 -0600

    edited_homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,13 +5,13 @@ export default function Home() {
     <div>
       <h1>About Page</h1>
       <p>
-I am a client care professional in massage therapy working to
-leverage my existing skills for a new career in web design.
-With over 8 years of experience in assessing clients’ needs and
-designing treatment plans to fit their requirements, 
-I am excited to transition these skills into designing websites 
-for clients. As an enthusiastic communicator and quick learner, 
-I am excited to work on projects’ as a Full Stack Web Developer. 
+        I am a client care professional in massage therapy working to
+        leverage my existing skills for a new career in web design.
+        With over 8 years of experience in assessing clients’ needs and
+        designing treatment plans to fit their requirements, 
+        I am excited to transition these skills into designing websites 
+        for clients. As an enthusiastic communicator and quick learner, 
+        I am excited to work on projects’ as a Full Stack Web Developer. 
 
 </p>
 
