fe666640ad557bbf00ed7fdf37eabf1ea60d1774
ErinJordan222 <erin.jordan@du.edu>
2022-06-14 15:52:58 -0600

    added_projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,3 +1,162 @@
+import React from 'react';
+import Quiz from '../../assets/codequiz.png';
+import Schedule from '../../assets/schedular.png';
+import Weather from '../../assets/weather.png';
+import Generator from '../../assets/readmegenerator.png';
+import TeamGenerator from '../../assets/teamgenerator.png';
+import Note from '../../assets/notetaker.png';
+import Ecommerce from '../../assets/ecommerce.png';
+import Blog from '../../assets/techblog.png'; 
+import SocialNetwork from '../../assets/socialnetworkapi.png';
+
+export default function Projects() {
+    return (
+        <div>
+            <div className="card">
+                <div className="img-container">
+                <img src="../../assets/codequiz.png"></img>
+                </div>
+                <div className="content">
+                    <ul>
+                        <li>
+                        <p className="project-title">Coder Quiz</p>
+                        </li>
+                        <li>
+                        <p className="project-description">
+                            Description here
+                        </p>
+                        </li>
+                        <li>
+                        <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/code_quiz.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            <a href="https://erinjordan222.github.io/code_quiz/"><btn type="codequiz"></btn><img src='../../assets/codequiz.png'></img></a>
+                        </div>
+                        </li>
+                        <li>
+                        <p className="project-title">Schedule App</p>
+                        </li>
+                        <li>
+                            <p className="project-description">
+                                Description Here
+                                </p>
+                        </li>
+                        <li>
+                        <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/ScheduleApp.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            <a href="https://erinjordan222.github.io/ScheduleApp/"><btn type="scheduleapp"></btn><img src='../../assets/schedular.png'></img></a>
+                        </div>
+                        </li>
+                        <li>
+                            <p className="project-title">ReadMe Generator
+                            </p>
+                        </li>
+                        <li>
+                            <p className="project-description">
+                                Description Here
+                                </p>
+                        </li>
+                        <li>
+                            <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/README_Generator.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            {/* <a href="https://erinjordan222.github.io/ScheduleApp/"><btn type="readmegenerator"></btn><img src='../../assets/readmegenerator.png'></img></a> */}
+                            </div>
+                        </li>
+                        <li>
+                            <p className="project-title">Weather Dashboard
+                            </p>
+                        </li>
+                        <li>
+                            <p className="project-description">
+                                Description Here
+                                </p>
+                        </li>
+                        <li>
+                            <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/Weather_Dashboard.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            <a href="https://erinjordan222.github.io/Weather_Dashboard/"><btn type="weather"></btn><img src='../../assets/weather.png'></img></a>
+                            </div>
+                            </li>
+                            <li>
+                        <p className="project-title">Team Generator</p>
+                        </li>
+                        <li>
+                        <p className="project-description">
+                            Description here
+                        </p>
+                        </li>
+                        <li>
+                        <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/Team_Profile_Generator.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            {/* <a href="https://erinjordan222.github.io/code_quiz/"><btn type="codequiz"></btn><img src='../../assets/codequiz.png'></img></a> */}
+                        </div>
+                        </li>
+                        <li>
+                        <p className="project-title">Note Taker</p>
+                        </li>
+                        <li>
+                        <p className="project-description">
+                            Description here
+                        </p>
+                        </li>
+                        <li>
+                        <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/Note_Pad_11.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            <a href="https://note-pad-du-11.herokuapp.com/"><btn type="codequiz"></btn><img src='../../assets/notetaker.png'></img></a>
+                        </div>
+                        </li>
+                        <li>
+                        <p className="project-title">E-Commerce Backend</p>
+                        </li>
+                        <li>
+                        <p className="project-description">
+                            Description here
+                        </p>
+                        </li>
+                        <li>
+                        <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/e-commerce.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            {/* <a href="https://note-pad-du-11.herokuapp.com/"><btn type="codequiz"></btn><img src='../../assets/notetaker.png'></img></a> */}
+                        </div>
+                        </li>
+                        <li>
+                        <p className="project-title">Tech Blog</p>
+                        </li>
+                        <li>
+                        <p className="project-description">
+                            Description here
+                        </p>
+                        </li>
+                        <li>
+                        <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/tech-blog.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            <a href="https://tech-blog-du-14-2022.herokuapp.com/"><btn type="codequiz"></btn><img src='../../assets/techblog.png'></img></a>
+                        </div>
+                        </li>
+                        <li>
+                        <p className="project-title">Social Network API</p>
+                        </li>
+                        <li>
+                        <p className="project-description">
+                            Description here
+                        </p>
+                        </li>
+                        <li>
+                        <div className="project-icons">
+                            <a href="https://github.com/ErinJordan222/Social-Network_API.git" img src='../../assets/github.png' alt="github-icon"></a>
+                            {/* <a href="https://erinjordan222.github.io/code_quiz/"><btn type="codequiz"></btn><img src='../../assets/codequiz.png'></img></a> */}
+                        </div>
+                        </li>
+                            
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
+
 // Code Quiz
 // Schedular
 // Weather
